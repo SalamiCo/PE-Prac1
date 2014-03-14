3b7ba207e970699db9139329500162d287a75916
Daniel Escoz <darkhogg@gmail.com>
2014-03-14 16:54:40 +0100

    Implemented elitism steps

--- src/pe1314/g11/util/ElitismStepPair.java
+++ src/pe1314/g11/util/ElitismStepPair.java
@@ -1,9 +1,12 @@
 package pe1314.g11.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import pe1314.g11.Chromosome;
+import pe1314.g11.Problem;
 import pe1314.g11.SolverStep;
 
 public final class ElitismStepPair<V, C extends Chromosome<C>> {
@@ -19,8 +22,8 @@ public final class ElitismStepPair<V, C extends Chromosome<C>> {
     }
 
     /**
-     * Returns a {@link ProblemStep} that saves the best chromosomes of the input population and does nothing to the
-     * output.
+     * Returns a {@link ProblemStep} that saves the best chromosomes of the input population. The output is returned
+     * sorted as a side effect.
      *
      * @return The saving step of the elitism implementation
      */
@@ -30,7 +33,7 @@ public final class ElitismStepPair<V, C extends Chromosome<C>> {
 
     /**
      * Returns a {@ProbemStep} that resets the previously saved chromosomes to the output population,
-     * removing the worst of them.
+     * removing the worst of them. The output is returned partially sorted as a side effect.
      *
      * @return The restoring step of the elitism implementation
      */
@@ -40,9 +43,34 @@ public final class ElitismStepPair<V, C extends Chromosome<C>> {
 
     private class SaveStep implements SolverStep<V,C> {
 
+        @Override
+        public void apply (Problem<V,C> problem, List<C> input, Random random, int generation, List<C> output) {
+            output.addAll(input);
+            Collections.sort(output, new FitnessComparator<C>(problem));
+
+            final int eliteNum = (int) (output.size() * percent);
+
+            elite.clear();
+            for (int i = 0; i < eliteNum; i++) {
+                elite.add(output.get(i));
+            }
+        }
+
     }
 
     private class RestoreStep implements SolverStep<V,C> {
+
+        @Override
+        public void apply (Problem<V,C> problem, List<C> input, Random random, int generation, List<C> output) {
+            output.addAll(input);
+            Collections.sort(output, new FitnessComparator<C>(problem));
+
+            final int eliteNum = (int) (output.size() * percent);
+
+            for (int i = 0; i < eliteNum; i++) {
+                output.set(output.size() - i - 1, elite.get(i));
+            }
+        }
 
     }
 }
