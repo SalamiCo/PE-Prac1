f09d86b1195a84ffb94f06318c0a4f9c0584af70
Pedro Morgado <pedro@riderstate.com>
2014-04-09 14:55:16 +0200

    Added Permutation Chromosome

---
+++ src/pe1314/g11/sga/PermutationChromosome.java
@@ -0,0 +1,53 @@
+package pe1314.g11.sga;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import pe1314.g11.Chromosome;
+
+public final class PermutationChromosome extends Chromosome<PermutationChromosome> {
+
+    private final List<Integer> permutation;
+
+    public PermutationChromosome (List<Integer> permutation) {
+        this.permutation = Collections.unmodifiableList(new ArrayList<>(permutation));
+    }
+
+    @Override
+    public int getMutationPlaces () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getMutationTypes () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public PermutationChromosome getMutated (int type, int place) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getCombinationPlaces () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getCombinationTypes () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public PermutationChromosome getCombined (PermutationChromosome other, int type, int place) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
